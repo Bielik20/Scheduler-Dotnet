a028ba71f2e05e5308919b6ccc77f3171f54942f
Bielik20 <bielik20@gmail.com>
2017-04-15 13:03:53 +0200

    Add rest of tutorial

--- Scheduler.API/Startup.cs
+++ Scheduler.API/Startup.cs
@@ -5,33 +5,120 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Scheduler.API.Core;
+using Scheduler.Data;
+using Scheduler.Data.Abstract;
+using Scheduler.Data.Repositories;
+using Newtonsoft.Json.Serialization;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using AutoMapper;
 
 namespace Scheduler.API
 {
     public class Startup
     {
+        private static string _applicationPath = string.Empty;
+        private static string _contentRootPath = string.Empty;
+        public IConfigurationRoot Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            _applicationPath = env.WebRootPath;
+            _contentRootPath = env.ContentRootPath;
+            // Setup configuration sources.
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_contentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // This reads the configuration keys from the secret store.
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets<Startup>();
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SchedulerContext>(options =>
+                options.UseSqlServer(Configuration["Data:SchedulerConnection:ConnectionString"],
+                b => b.MigrationsAssembly("Scheduler.Data")));
+
+            // Repositories
+            services.AddScoped<IScheduleRepository, ScheduleRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IAttendeeRepository, AttendeeRepository>();
+            
+            // Automapper Configuration
+            services.AddAutoMapper();
+
+            // Enable Cors
+            services.AddCors();
+
+            // Add MVC services to the services container.
+            services.AddMvc()
+                .AddJsonOptions(opts =>
+                {
+                    // Force Camel Case to JSON
+                    opts.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
+            app.UseStaticFiles();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // Add MVC to the request pipeline.
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
 
-            app.Run(async (context) =>
+            app.UseExceptionHandler(
+              builder =>
+              {
+                  builder.Run(
+                    async context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+
+                            await context.Response.WriteAsync(error.Error.Message).ConfigureAwait(false);
+                        }
+                    });
+              });
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                //routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
             });
+
+            SchedulerDbInitializer.Initialize(app.ApplicationServices);
         }
     }
 }
